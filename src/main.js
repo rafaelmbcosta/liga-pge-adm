4926f4b1426db2fc7cdfb55cdf1a784088516f07
Rafael Costa <rafael.costa@pge.ce.gov.br>
2018-09-27 17:52:35 -0300

    preparing to get restricted content

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueLocalStorage from 'vue-localstorage'
+import axios from './helpers/axios'
 
 Vue.config.productionTip = false
 Vue.use(VueLocalStorage)
 
 new Vue({
   router,
+  axios,
   render: h => h(App)
 }).$mount('#app')
