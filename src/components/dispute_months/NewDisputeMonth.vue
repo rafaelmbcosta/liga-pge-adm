c3183aaa1c3e8b7bdf09426a6d4c1f251dfb4f06
Rafael Costa <rafael.costa@pge.ce.gov.br>
2019-01-07 17:50:57 -0300

    validando form

--- src/components/dispute_months/NewDisputeMonth.vue
+++ src/components/dispute_months/NewDisputeMonth.vue
@@ -1,14 +1,22 @@
 <template lang="html">
-  <div class="col-4">
+  <div class="col-4 offsettatus-4">
     {{ nome }}
     <div class="card">
       <div class="card-header alert-success">Novo mês de disputa </div>
       <div class="card-body">
-        <form class="form-inline">
+        <form class="form-inline"
+              @submit="checkForm"
+              action="test2">
+          <p v-if="errors.length">
+            <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
           <div class="form-group">
             <label class="form-inline">Nome:&nbsp;</label>
             <input type="text" class="form-control" v-model="nome"/>&nbsp;
-            <button type="submit" name="button" class="btn btn-success"> adicionar </button>
+            <input type="submit" name="button" class="btn btn-success" value="Enviar" />
           </div>
         </form>
       </div>
@@ -20,11 +28,33 @@
 export default {
   data () {
     return {
-      nome: null
+      nome: null,
+      errors: []
+    }
+  },
+  methods: {
+    test () {
+      alert('iei!')
+    },
+    test2 () {
+      alert('Opa, validação de action')
+    },
+    checkForm (e) {
+      if (this.nome.length === 0 || this.nome === '') {
+        this.errors.push('Nome é obrigatório')
+      }
+
+      if (!this.errors.length) {
+        alert('retornando true')
+        return true
+      }
+
+      e.preventDefault()
     }
   }
 }
 </script>
 
 <style lang="css">
+
 </style>
