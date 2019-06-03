642268ce9667754c9cbb4c489bea875731eb5be7
Rafael Costa <rafaelcosta@16935L.pge.ce.gov.br>
2019-06-03 17:25:37 -0300

    team edit and create

--- src/api/server/index.js
+++ src/api/server/index.js
@@ -3,14 +3,27 @@ import axios from 'axios'
 export default {
   serviceLogin (email, password) {
     console.log('Entrou no Valendo!')
-    return axios.post(process.env.VUE_APP_API_CLIENT+'/user_token', {
+    return axios.post(process.env.CUSTOM_API_ADDRESS+'/user_token', {
       auth: {
         email: email,
         password: password
       }
     })
   },
+  addTeam (team) {
+    return axios.post(process.env.CUSTOM_API_ADDRESS+'/teams/create', {
+      team: team
+    })
+  },
+  teamActivation(team, value) {
+    return axios.post(process.env.CUSTOM_API_ADDRESS+'/teams/activation', {
+      team: {
+        id: team.id,
+        active: value
+      }
+    })
+  },
   getTeams () {
-    return axios.get(process.env.VUE_APP_API_CLIENT+'/api/v1/teams')
+    return axios.get(process.env.CUSTOM_API_ADDRESS+'/teams')
   }
 }
