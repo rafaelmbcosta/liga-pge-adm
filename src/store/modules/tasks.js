ba6b6fa650848e68a298f2275c6ba8866cce5fca
Rafael Costa <rafaelmbcosta@gmail.com>
2019-08-27 01:53:53 -0300

    wip tasks

--- src/store/modules/tasks.js
+++ src/store/modules/tasks.js
@@ -10,7 +10,20 @@ const getters = {
 }
 
 const mutations = {
+  RUN_TASK(task){
+    let url = {
+      'closed_market': '/api/v1/closed_market_routines',
+      'round_finished': '/api/v1/round_finished_routines',
+      'currencies': '/api/v1/currencies/rerun'
+    }
+    service.runTask(url[task])
+      .then(response => {
 
+      })
+      .catch(error => {
+        
+      })
+  }
 }
   
 const actions = {
--- src/views/TasksLog.vue
+++ src/views/TasksLog.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <TaskList />
-    <Logs />
   </div>
 </template>
 
